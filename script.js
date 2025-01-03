de2a66531b2161ad926bfbfb72a0689ddc7d38c4
RokyGamerz <nguyennamclone69@gmail.com>
2025-01-03 21:47:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.classList.toggle('dark-mode');
     }, 3000); // Chuyển đổi hiệu ứng sáng tối mỗi 3 giây
 });
+<script>
+    function copyUsername() {
+        const username = "@nem_nuong.";
+        navigator.clipboard.writeText(username).then(() => {
+            alert(`Copied to clipboard: ${username}`);
+        }).catch(err => {
+            console.error("Failed to copy text: ", err);
+        });
+    }
+</script>
